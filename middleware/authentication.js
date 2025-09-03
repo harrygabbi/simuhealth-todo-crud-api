1c9bf293b52143ecd107f796b2cfecd4c993b286
harry <gabbiharkaran@gmail.com>
2025-09-03 16:11:21 -0700

    Added middleware for login puroposes. \nCOmpleted ToDo APIs changed them so they can handle filter and now delete and update only works if you are the owner.

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,4 +1,21 @@
 
 export function authMiddleware(req, res, next){
-    
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header is missing' });
+    }
+
+    // USING THE THE NODE BUFFER TO CONVERT TOKEN INTO PLAIN TEXT
+    const [bearer, token] = authHeader.split(' ');
+    if (bearer !== 'Bearer' || !token) {
+        return res.status(401).json({ error: 'Invalid Authorization format' });
+    }
+
+    try {
+        const username = Buffer.from(token, 'base64').toString('utf8');
+        req.user = { username };
+        next();
+    } catch (err) {
+        return res.status(401).json({ error: 'Invalid token' });
+    }
 }
--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,23 +1,53 @@
 import express from 'express' ;
+import { authMiddleware } from '../middleware/authentication.js';
+
+
 
 const Router = express.Router();
-const example_TODO_test = [
-    { id: 1, description: 'Buy groceries' },
-    { id: 2, description: 'Learn Node.js' },
-];
+
+// LOGIN BEFORE CRUD TODO LIST
+Router.use(authMiddleware); 
+
+const example_TODO_test = [];
 
 Router.get('/', (req, res) => {
-  res.json(example_TODO_test);
+
+    const { description, owner, category } = req.query;
+    let filteredTodos = example_TODO_test;
+
+
+
+    // FILTER LISTS ON QUERY ON BASIS OF DESC OWNER OR CATEGORY
+    if (description) {
+        filteredTodos = filteredTodos.filter(todo => 
+        todo.description.toLowerCase().includes(description.toLowerCase())
+        );
+    }
+
+    if (owner) {
+        filteredTodos = filteredTodos.filter(todo => 
+        todo.owner.toLowerCase() === owner.toLowerCase()
+        );
+    }
+
+    if (category) {
+        filteredTodos = filteredTodos.filter(todo => 
+        todo.category.toLowerCase() === category.toLowerCase()
+        );
+    }
+
+    res.json(filteredTodos);
 });
 
 Router.post('/', (req, res) => {
-    const {description} = req.body;
+    const {description, category} = req.body;
     
     if(!description){
-        res.status(404).json( {error: 'Description is missing '});
+        res.status(400).json( {error: 'Description is missing '});
     }
 
-    const newTodo = { id: (example_TODO_test.length + 1), description };
+    // GENERATING NEW TODO
+    const newTodo = { id: (example_TODO_test.length + 1), description, category: category || 'General', owner: req.user.username,};
     example_TODO_test.push(newTodo);
     res.status(201).json(newTodo);
 
@@ -33,6 +63,10 @@ Router.put('/:id', (req, res) => {
         res.status(404).json({ error: 'todo couldnt found' });
     }
 
+    if(todo.owner !== req.user.username){
+        res.status(403).json( {error: 'You are not the owner. Cannot change content.'});
+    }
+
     if(!description){
         res.status(404).json( {error: 'Description is missing '});
     }
@@ -46,11 +80,17 @@ Router.delete('/:id', (req, res) => {
     const TodoID = parseInt(req.params.id);
     const todo = example_TODO_test.find( t => t.id === TodoID );
 
-    if(todo == -1) {
+    if(!todo) {
         res.status(404).json({ error: 'todo couldnt found' });
     }
 
-    example_TODO_test.splice(todo, 1);
+    if(todo.owner !== req.user.username){
+        res.status(403).json({ error: 'you cannot delete someone elses made todo.' });
+    }
+
+    // REMOVING TODO FROM LIST
+    const todoIndex = example_TODO_test.indexOf(todo);
+    example_TODO_test.splice(todoIndex, 1);
     res.status(204).send();
     
 });
